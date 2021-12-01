d29c8f101599db62c3a753dba8dd7afd9b2a5ec8
Jonathan <omgitsjt@uw.edu>
2021-12-01 14:57:52 -0800

    neighborhood data attempt

--- final-visualization/main.js
+++ final-visualization/main.js
@@ -2,3 +2,20 @@ var map = L.map('map').setView([47.6062, -122.3321], 11);
 var tileLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
 tileLayer.addTo(map)
 
+// get neighborhoodData
+var neighborhoodData;
+
+// retreived from https://github.com/seattleio/seattle-boundaries-data/tree/9c56894e67e3fb0faa3219efb6a0d66312298428
+let promise = fetch("https://raw.githubusercontent.com/seattleio/seattle-boundaries-data/9c56894e67e3fb0faa3219efb6a0d66312298428/data/neighborhoods.geojson");
+promise.then(function(response) {
+    return response.json();
+}).then(function(data) {
+    console.log(data);
+    neighborhoodData = data;
+})
+.catch(function(error) {
+    console.log("Error");
+    console.log(error);
+})
+
+var geojsonLayer = L.geoJSON(neighborhoodData).addTo(map);
