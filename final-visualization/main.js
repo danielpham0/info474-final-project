c51dce4b66e632e8f38097be23f6a2343a0ca3ca
Daniel Pham <danielpham0@gmail.com>
2021-12-01 14:41:08 -0800

    merge

--- final-visualization/main.js
+++ final-visualization/main.js
@@ -1,9 +1,4 @@
-var map = L.map('map').setView([47.6062, -122.3321], 10);
-//var tileLayer = L.tileLayer(
-    
-    mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-L.tileLayer(
-        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; ' + mapLink + ' Contributors',
-            maxZoom: 18,
-            }).addTo(map);
+var map = L.map('map').setView([47.6062, -122.3321], 11);
+var tileLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
+tileLayer.addTo(map)
+
